050f103f2ab0b0f5bf917935dab79631a7cb4b27
Harry Chopra <harrychopra@gmail.com>
2019-11-09 21:17:15 +0000

    Working

---
+++ Doubly-linked-list-node.js
@@ -0,0 +1,10 @@
+//Reviewed. Status: Done.
+//(Stretch: Tests Due) 
+
+module.exports = class DoublyLinkedListNode {
+  constructor(data, next = null, prev = null) {
+    this.data = data;
+    this.next = next;
+    this.prev = prev;
+  }
+};
