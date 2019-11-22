01a34124c3af869467457340a99f6d08ebd46228
Harry Chopra <harrychopra@gmail.com>
2019-11-22 18:17:17 +0000

    Working

---
+++ Priority-queue-node.js
@@ -0,0 +1,11 @@
+//Reviewed. Status: Done.
+//(Stretch: Tests Due) 
+
+// Each Node has a value and a priority.
+
+module.exports = class PriorityQueueNode {
+  constructor(value, priority) {
+    this.value = value;
+    this.priority = priority;
+  }
+};
