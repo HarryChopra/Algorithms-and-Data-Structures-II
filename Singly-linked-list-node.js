dfe6c5be4b29021e1a4faecbb4a9edf7a6e24646
Harry Chopra <harrychopra@gmail.com>
2019-12-02 04:17:19 +0000

    Working

---
+++ Singly-linked-list-node.js
@@ -0,0 +1,9 @@
+//Reviewed. Status: Done.
+//(Stretch: Tests Due) 
+
+module.exports = class SinglyLinkedListNode {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+};
