e5eb90c28ea02d4e31015983407dc11503bbd2b1
Harry Chopra <harrychopra@gmail.com>
2020-01-21 10:17:28 +0000

    Working

---
+++ capitalize-words.js
@@ -0,0 +1,19 @@
+//Reviewed. Status: Done.
+//(Stretch: Tests Due) 
+
+// capitalizeWords
+// Write a recursive function called capitalizeWords.
+// Given an array of words, return a new array containing each word capitalized.
+
+function capitalizeWords(arr) {
+  const result = [];
+
+  if (!arr.length) return result;
+
+  result.push(arr[0].toUpperCase());
+
+  return [...result, ...capitalizeWords(arr.slice(1))];
+}
+
+const words = ['i', 'am', 'learning', 'recursion'];
+console.log(capitalizeWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']
