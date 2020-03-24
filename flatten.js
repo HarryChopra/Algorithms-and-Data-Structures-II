cf34a32ad40e83432fa26941021e45a656d83b97
Harry Chopra <harrychopra@gmail.com>
2020-03-24 17:17:40 +0000

    Working

---
+++ flatten.js
@@ -0,0 +1,22 @@
+//Reviewed. Status: Done.
+//(Stretch: Tests Due) 
+
+// flatten
+// Write a recursive function called flatten which accepts an array of arrays
+// and returns a new array with all values flattened.
+
+function flatten(arr) {
+  let resultArr = [];
+
+  for (const item of arr) {
+    if (item instanceof Array) resultArr = resultArr.concat(flatten(item));
+    else resultArr.push(item);
+  }
+
+  return resultArr;
+}
+
+console.log(flatten([1, 2, 3, [4, 5]])); // [1, 2, 3, 4, 5]
+console.log(flatten([1, [2, [3, 4], [[5]]]])); // [1, 2, 3, 4, 5]
+console.log(flatten([[1], [2], [3]])); // [1, 2, 3]
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])); // [1, 2, 3]
