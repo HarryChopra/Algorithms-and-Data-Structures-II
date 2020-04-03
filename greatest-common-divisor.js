b18324c37dbad6b006ef415ab17d43a3ee2e6923
Harry Chopra <harrychopra@gmail.com>
2020-04-03 13:17:42 +0100

    Working

---
+++ greatest-common-divisor.js
@@ -0,0 +1,17 @@
+//Reviewed. Status: Done.
+//(Stretch: Tests Due) 
+
+// Write a function that returns the greatest common divisor of two integers.
+
+function gcd(a, b) {
+  while (a && b) {
+    a %= b;
+    b = a ? b % a : b;
+  }
+
+  return Math.abs(a || b);
+}
+console.log(gcd(-1300, 250)); // 50
+console.log(gcd(111, 13)); // 1
+console.log(gcd(1000, 25)); // 25
+console.log(gcd(111, 15)); // 3
